d420c25d7779c15d6d9332f84257f8b0f6a78cde
obrockmole <86027400+obrockmole@users.noreply.github.com>
2026-07-27 15:55:58 -0500

    Desktop logging colors

--- shared/src/jvmMain/kotlin/com/obrockmole/kmpbetterdining/utils/Logger.jvm.kt
+++ shared/src/jvmMain/kotlin/com/obrockmole/kmpbetterdining/utils/Logger.jvm.kt
@@ -1,5 +1,18 @@
 package com.obrockmole.kmpbetterdining.utils
 
 actual fun writePlatformLog(logLevel: LogLevel, tag: String, message: String) {
-    println("[${logLevel.name}] $tag: $message")
+    val white = ""
+    val grey = "\u001B[38;5;250m"
+    val yellow = "\u001B[33m"
+    val red = "\u001B[31m"
+    val reset = "\u001B[0m"
+
+    val color = when (logLevel) {
+        LogLevel.INFO -> white
+        LogLevel.DEBUG -> grey
+        LogLevel.WARN -> yellow
+        LogLevel.ERROR -> red
+    }
+
+    println("${color}[${logLevel.name}] $tag: $message${reset}")
 }
